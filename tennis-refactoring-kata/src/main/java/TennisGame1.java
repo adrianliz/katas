8c4166ff695b02cc357c3be176fb19c2dd6ab7b5
Adrián Lizaga <lizagaadrian@gmail.com>
2023-11-17 22:50:53 +0100

    refactor: remove redundant else

--- tennis-refactoring-kata/src/main/java/TennisGame1.java
+++ tennis-refactoring-kata/src/main/java/TennisGame1.java
@@ -31,10 +31,16 @@ public class TennisGame1 implements TennisGame {
     }
     if (m_score1 >= 4 || m_score2 >= 4) {
       int minusResult = m_score1 - m_score2;
-      if (minusResult == 1) score = "Advantage player1";
-      else if (minusResult == -1) score = "Advantage player2";
-      else if (minusResult >= 2) score = "Win for player1";
-      else score = "Win for player2";
+      if (minusResult == 1) {
+        return "Advantage player1";
+      }
+      if (minusResult == -1) {
+        return "Advantage player2";
+      }
+      if (minusResult >= 2) {
+        return "Win for player1";
+      }
+      return "Win for player2";
     } else {
       for (int i = 1; i < 3; i++) {
         if (i == 1) tempScore = m_score1;
