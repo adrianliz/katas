d618629b1ab889108d9a89137d99177c233e5f50
Adrián Lizaga <lizagaadrian@gmail.com>
2023-11-18 00:25:34 +0100

    refactor: use chain of responsibility pattern

--- tennis-refactoring-kata/src/main/java/TennisGame1.java
+++ tennis-refactoring-kata/src/main/java/TennisGame1.java
@@ -1,3 +1,5 @@
+import java.util.Optional;
+
 public class TennisGame1 implements TennisGame {
   private Player server;
   private Player receiver;
@@ -16,43 +18,86 @@ public class TennisGame1 implements TennisGame {
   }
 
   public String getScore() {
-    if (server.hasWon(receiver)) {
-      return "Win for player1";
-    }
-    if (receiver.hasWon(server)) {
-      return "Win for player2";
-    }
-    if (server.hasAdvantage(receiver)) {
-      return "Advantage player1";
-    }
-    if (receiver.hasAdvantage(server)) {
-      return "Advantage player2";
-    }
-    if (server.isInDeuce(receiver)) {
-      return "Deuce";
-    }
-    if (server.hasSameScore(receiver)) {
-      return formatEqualScore();
-    }
-    return formatPlayerScore(server.getScore()) + "-" + formatPlayerScore(receiver.getScore());
+    return new ServerWonScore()
+        .calculate(server, receiver)
+        .or(() -> new ReceiverWonScore().calculate(server, receiver))
+        .or(() -> new ServerAdvantageScore().calculate(server, receiver))
+        .or(() -> new ReceiverAdvantageScore().calculate(server, receiver))
+        .or(() -> new DeuceScore().calculate(server, receiver))
+        .or(() -> new EqualScore().calculate(server, receiver))
+        .or(() -> new RegularScore().calculate(server, receiver))
+        .orElseThrow(() -> new IllegalStateException("Invalid score."));
   }
 
-  private String formatEqualScore() {
-    return switch (server.getScore()) {
-      case 0 -> "Love-All";
-      case 1 -> "Fifteen-All";
-      case 2 -> "Thirty-All";
-      default -> "Deuce";
-    };
+  private interface Score {
+    Optional<String> calculate(final Player server, final Player receiver);
   }
 
-  private String formatPlayerScore(int playerScore) {
-    return switch (playerScore) {
-      case 0 -> "Love";
-      case 1 -> "Fifteen";
-      case 2 -> "Thirty";
-      case 3 -> "Forty";
-      default -> "";
-    };
+  private static class ServerWonScore implements Score {
+    @Override
+    public Optional<String> calculate(final Player server, final Player receiver) {
+      return server.hasWon(receiver) ? Optional.of("Win for player1") : Optional.empty();
+    }
+  }
+
+  private static class ReceiverWonScore implements Score {
+    @Override
+    public Optional<String> calculate(final Player server, final Player receiver) {
+      return receiver.hasWon(server) ? Optional.of("Win for player2") : Optional.empty();
+    }
+  }
+
+  private static class ServerAdvantageScore implements Score {
+    @Override
+    public Optional<String> calculate(final Player server, final Player receiver) {
+      return server.hasAdvantage(receiver) ? Optional.of("Advantage player1") : Optional.empty();
+    }
+  }
+
+  private static class ReceiverAdvantageScore implements Score {
+    @Override
+    public Optional<String> calculate(final Player server, final Player receiver) {
+      return receiver.hasAdvantage(server) ? Optional.of("Advantage player2") : Optional.empty();
+    }
+  }
+
+  private static class DeuceScore implements Score {
+    @Override
+    public Optional<String> calculate(final Player server, final Player receiver) {
+      return server.isInDeuce(receiver) ? Optional.of("Deuce") : Optional.empty();
+    }
+  }
+
+  private static class EqualScore implements Score {
+    @Override
+    public Optional<String> calculate(Player server, Player receiver) {
+      return server.hasSameScore(receiver)
+          ? Optional.of(
+              switch (server.getScore()) {
+                case 0 -> "Love-All";
+                case 1 -> "Fifteen-All";
+                case 2 -> "Thirty-All";
+                default -> "Deuce";
+              })
+          : Optional.empty();
+    }
+  }
+
+  private static class RegularScore implements Score {
+    @Override
+    public Optional<String> calculate(Player server, Player receiver) {
+      return Optional.of(
+          formatPlayerScore(server.getScore()) + "-" + formatPlayerScore(receiver.getScore()));
+    }
+
+    private String formatPlayerScore(int playerScore) {
+      return switch (playerScore) {
+        case 0 -> "Love";
+        case 1 -> "Fifteen";
+        case 2 -> "Thirty";
+        case 3 -> "Forty";
+        default -> throw new IllegalArgumentException("Invalid player score.");
+      };
+    }
   }
 }
