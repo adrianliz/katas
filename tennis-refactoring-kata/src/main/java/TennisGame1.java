8c712a6b7d34e937330981dfeae2c95deb4eb6a8
Adrián Lizaga <lizagaadrian@gmail.com>
2023-11-17 22:39:04 +0100

    refactor: remove inline player name

--- tennis-refactoring-kata/src/main/java/TennisGame1.java
+++ tennis-refactoring-kata/src/main/java/TennisGame1.java
@@ -11,7 +11,7 @@ public class TennisGame1 implements TennisGame {
   }
 
   public void wonPoint(String playerName) {
-    if (playerName == "player1") m_score1 += 1;
+    if (player1Name.equals(playerName)) m_score1 += 1;
     else m_score2 += 1;
   }
 
